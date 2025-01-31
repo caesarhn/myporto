ac1bce4566c07b7803cfab9b24ed89bb3cbac252
caesar6747 <caesarhnc100@gmail.com>
2025-01-31 21:04:56 +0700

    update daily

--- loadtest.js
+++ loadtest.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 1000, // Jumlah virtual users
-    duration: '30s', // Durasi pengujian
+    vus: 10, // Jumlah virtual users
+    duration: '10s', // Durasi pengujian
 };
 
 export default function () {
-    http.get('http://localhost/creator/read/3081a1af-1ca5-434b-8bf1-f78c3275c2a2'); // Ganti PORT dengan port aplikasi Anda
-    sleep(1);
+    http.get('https://engineeringsampel.com/beranda'); // Ganti PORT dengan port aplikasi Anda
+    sleep(10);
 }
--- src/middleware.js
+++ src/middleware.js
@@ -7,12 +7,16 @@ import { v4 as uuid } from 'uuid'
 import bcrypt from 'bcrypt'
 
 export const onRequest = defineMiddleware(async (context, next) => {
+    const path = context.url.pathname.split('/')
     const {request, locals} = context
     const userAgent = request.headers.get('user-agent') || '';
     const isMobile = /mobile/i.test(userAgent);
     locals.device = isMobile ? 'mobile' : 'desktop';
 
-    const path = context.url.pathname.split('/')
+    if(path[2] == 'read' || PUBLIC_URL.includes(context.url.pathname)){
+        return next()
+    }
+
     const token = context.cookies.get("token")
     const findSessions = await db.select().from(Session).where(eq(Session.sessionId, token?.value != null ? token.value : ""))
 
