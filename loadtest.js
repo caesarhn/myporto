4fabbcebbf1154098ae1ab4c6ee4c7691246e85a
caesar6747 <caesarhnc100@gmail.com>
2025-02-09 01:52:49 +0700

    arrange image

--- loadtest.js
+++ loadtest.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 100, // Jumlah virtual users
+    vus: 10, // Jumlah virtual users
     duration: '12s', // Durasi pengujian
 };
 
 export default function () {
-    http.get('https://engineeringsampel.com/creator/read/b0aa10d2-bc54-4c21-95d2-c8f6f53a8ed1'); // Ganti PORT dengan port aplikasi Anda
+    http.get('https://engineeringsampel.com/Caesar%20Nuari/read/3081a1af-1ca5-434b-8bf1-f78c3275c2a2'); // Ganti PORT dengan port aplikasi Anda
     sleep(10);
 }
